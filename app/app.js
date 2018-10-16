5eb698e50f2ae96fac2f40f11a5a9fdcf18b0687
alicesilva <alice.silva@ccc.ufcg.edu.br>
2018-10-15 21:53:31 -0300

    primeira parte terminada e inicio da segunda parte

--- app/app.js
+++ app/app.js
@@ -3,9 +3,14 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', []).controller('HomeCrtl', ['$scope', function($scope){
   $scope.functions = false;
+  $scope.numberOfProjects = "";
+  $scope.postsColletedPerMonth = "";
+  $scope.trackesAnalyticsProfile = "";
+  $scope.CRMProfiles = "";
   $scope.viewFunctions = function(){
     $scope.functions = true;
     console.log($scope.functions)
     console.log("Oi");
   }
+  
 }]);
